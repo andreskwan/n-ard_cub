881462471c45f6fc5f1ce0d10e908d367c52dd6f
ANDRES KWAN <andreskwan@gmail.com>
2014-12-30 10:35:42 -0500

    NPM - Package.json - update versions - could damage the project

--- server.js
+++ server.js
@@ -1,64 +1,47 @@
 //--------------------------------------------
-//socket.io
-var io = require('socket.io').listen(8000);
-
-io.sockets.on('connection', function (socket) {
-  socket.broadcast.emit('join', socket['id']);
-  console.log(socket['id'] + ' has connected!');
-
-  socket.on('location', function (data){
-    socket.broadcast.emit('update', (socket['id'] + data)); 
-  });
-
-  socket.on('disconnect', function (){
-    socket.broadcast.emit('dissapear', (socket['id'])); 
-    console.log(socket['id'] + ' has disconnected!');
-  });
-
-    // socket.on('message', function (msg) {
-    //     console.log("msg" + msg);
-    //   });
-    // socket.on('disconnect', function () {
-    //     console.log("disconnect");
-    //   });
-});
-
 //Serial Port
 var serialport = require("serialport");
 var SerialPort = serialport.SerialPort;
-var arduinoSP  = "arduion sp not defined";
+var async      = require('async');
+
+var arduinoSP  = "arduio sp not defined";
 //ToDO - distinguish from OS X and linux
-  //require("serialport").list(function (err, ports) {
-    serialport.list(function (err, ports) {
+//require("serialport").list(function (err, ports) {
+// async.waterfall([
+    
+
+//blocking way 
+serialport.list(function (err, ports) {
     //console.log('JSON.stringify(ports)): \n' + JSON.stringify(ports));
     ports.forEach(function(port) {
-      console.log("--------------------------------------------");
-      // console.log("port name:    "+port.comName);
-      // console.log("pnpId:        "+port.pnpId);
-      // console.log("manufacturer: "+port.manufacturer);
+	console.log("--------------------------------------------");
+	// console.log("port name:    "+port.comName);
+	// console.log("pnpId:        "+port.pnpId);
+	// console.log("manufacturer: "+port.manufacturer);
 
-      if(port.manufacturer.trim() === "Arduino LLC"){
-          console.log("Arduino - Disponible en el puerto -- "+ port.comName);
-          console.log('JSON.stringify(port)): \n' + JSON.stringify(port));
-      //     var thenum = port.comName.replace(/[^0-9]/g,'');
-      //     console.log("theNum: "+thenum);
-          console.log("before: "+arduinoSP);
-      //     // var arraySplited = port.comName.split(".");
-      //     this.arduinoSP = "/dev/tty.usbmodem"+thenum;
-      // //     // console.log("############### "+arraySplited[1]+" ###############");
-          arduinoSP = port.comName;
-          console.log("after: "+arduinoSP);
-          // defPorts.resolve(puerto)
-          //--------------------------------------------
-    //sp module
-    var serialPortController = require('./app/controllers/serialPort.js');
-      console.log("server - arduinoSP: "+arduinoSP);
-      serialPortController(server,serialport,SerialPort,arduinoSP);
+	if(port.manufacturer.trim() === "Arduino LLC" ||
+            port.pnpId === "usb-Arduino_LLC_Arduino_Leonardo-if00"){
+            console.log("Arduino - Disponible en el puerto -- "+ port.comName);
+            console.log('JSON.stringify(port)): \n' + JSON.stringify(port));
+	    //     var thenum = port.comName.replace(/[^0-9]/g,'');
+	    //     console.log("theNum: "+thenum);
+            console.log("before: "+arduinoSP);
+	    //     // var arraySplited = port.comName.split(".");
+	    //     this.arduinoSP = "/dev/tty.usbmodem"+thenum;
+	    // //     // console.log("############### "+arraySplited[1]+" ###############");
+            arduinoSP = port.comName;
+            console.log("after: "+arduinoSP);
+            // defPorts.resolve(puerto)
+            //--------------------------------------------
+	    //sp module
+	    var serialPortController = require('./app/controllers/serialPort.js');
+	    console.log("server - arduinoSP: "+arduinoSP);
+	    serialPortController(server,serialport,SerialPort,arduinoSP);
         }else{
-          console.log("Arduino - no disponible en -- "+ port.comName);
+            console.log("Arduino - no disponible en -- "+ port.comName);
         }
     });
-  });
+});
 
 //--------------------------------------------
 //html template
@@ -85,8 +68,8 @@ server.set('views','./app/views/');
 server.http().io();
 //
 server.configure(function() {
-  //Para usar metodos de HTTP
-  server.use(express.bodyParser());
+    //Para usar metodos de HTTP
+    server.use(express.bodyParser());
 });
 
 //--------------------------------------------
@@ -97,7 +80,7 @@ server.use(express.static('./public'));
 //--------------------------------------------
 //server
 server.get('/', function (req, res) {
-  res.render('home');
+    res.render('home');
 });
 
 // //--------------------------------------------
@@ -111,5 +94,5 @@ server.use(function (req,res) {
     res.render('404', {url:req.url});
 });
 
-  
+
 server.listen(3000);
